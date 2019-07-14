9ec8e64f7799624bb2eac764d4be355cb09ebb03
Manfred Karrer <mk@nucleo.io>
2019-07-14 14:41:05 +0200

    Revert to master version (dont connect to all btc nodes)

--- core/src/main/java/bisq/core/app/BisqEnvironment.java
+++ core/src/main/java/bisq/core/app/BisqEnvironment.java
@@ -343,10 +343,10 @@ public class BisqEnvironment extends StandardEnvironment {
                 "Bisq";
         useAllProvidedNodes = commandLineProperties.containsProperty(BtcOptionKeys.USE_ALL_PROVIDED_NODES) ?
                 (String) commandLineProperties.getProperty(BtcOptionKeys.USE_ALL_PROVIDED_NODES) :
-                "true";
+                "false";
         numConnectionForBtc = commandLineProperties.containsProperty(BtcOptionKeys.NUM_CONNECTIONS_FOR_BTC) ?
                 (String) commandLineProperties.getProperty(BtcOptionKeys.NUM_CONNECTIONS_FOR_BTC) :
-                "50";
+                "9";
         ignoreLocalBtcNode = commandLineProperties.containsProperty(BtcOptionKeys.IGNORE_LOCAL_BTC_NODE) ?
                 (String) commandLineProperties.getProperty(BtcOptionKeys.IGNORE_LOCAL_BTC_NODE) :
                 "false";
